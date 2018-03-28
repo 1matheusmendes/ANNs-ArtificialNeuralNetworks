540ef45085a8426aa343b4f21ace6ec7c179db1a
1matheusmendes <matheus_mendess@live.com>
2018-03-28 15:50:33 -0300

    perceptron de uma camada

--- perceptron1.py
+++ perceptron1.py
@@ -11,8 +11,8 @@ pesos = [0.8, 0.1, 0] #PESOS "SINAPSES"
 def soma (e, p): #FUNÇÃO SOMA
     s = 0
     for i in range(3): #CORRENDO A LISTA, COM RANGE DE 3 POIS SÓ TEM 3 POSIÇÕES NO VETOR
-        #print(entradas[i])
-        #print(pesos[i])
+        print(entradas[i])
+        print(pesos[i])
         s += e[i] * p[i] #FUNÇÃO SOMA
     return s #RETORNA O VALOR DA SOMA        
         
@@ -24,5 +24,9 @@ def stepFunction(soma): #FUNÇÃO DEGRAU
     return 0 #CASO CONTRARIO RETORNA ZERO, REPRESENTA NADA
 
 r = stepFunction(s) #INSTANCIA DA FUNÇÃO DEGRAU
+
+print ("Resultado da função degrau")
+print (s)
+
   
 #teste Fetching latest commit…  
